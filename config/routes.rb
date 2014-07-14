cb7bb5c6c1da1411f30c0abc4137473d65ec3d3a
brandon <brandon@brandon-VirtualBox.(none)>
2014-07-13 18:16:19 -0700

    Working github login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :submissions
   root to: 'submissions#index'
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
+  devise_scope :user do
+  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+end# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
